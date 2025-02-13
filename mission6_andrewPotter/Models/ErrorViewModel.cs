2fc1442cbccf0ad70b304ef9b400db3804227023
AndrewJP01 <andrewpotter300@gmail.com>
2025-02-13 14:46:49 -0700

    Add project files.

---
+++ mission6_andrewPotter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission6_andrewPotter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
